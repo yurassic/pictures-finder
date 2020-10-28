90fa0a472c8c3b108ceac53ac175307638ee147d
yurassic <yuriynyay@gmail.com>
2020-10-28 12:56:10 +0200

    Add API logic

--- src/App.js
+++ src/App.js
@@ -1,7 +1,77 @@
+import { useState } from "react";
+import Grid from "@material-ui/core/Grid";
+import Box from "@material-ui/core/Box";
+import Container from "@material-ui/core/Container";
+import CircularProgress from "@material-ui/core/CircularProgress";
+import Alert from "@material-ui/lab/Alert";
+
+import SearchForm from "./components/SearchForm";
+import PicturesList from "./components/PicturesList";
+
+import { getUrl } from "./pixabayConfig";
+
 import "./App.css";
 
-function App() {
-  return <div className="App" />;
-}
+const App = () => {
+  const [isFetching, setIsFetching] = useState(false);
+  const [pictures, setPictures] = useState([]);
+  const [error, setError] = useState(null);
+  const [nothingFound, setNothingFound] = useState(false);
+
+  /* TODO:
+   * Add tests
+   * Refactor
+   * Deploy
+   */
+
+  const getPictures = (keyWord) => {
+    setIsFetching(true);
+    setError(null);
+
+    fetch(getUrl(keyWord))
+      .then((response) => response.json())
+      .then((result) => {
+        setIsFetching(false);
+        setNothingFound(result?.total === 0);
+        setPictures(result?.hits);
+      })
+      .catch((error) => {
+        setIsFetching(false);
+        setError(error?.message);
+      });
+  };
+
+  return (
+    <Container maxWidth="md">
+      <Grid
+        className="app-wrapper"
+        container
+        direction="column"
+        justify="center"
+        alignItems="center"
+      >
+        <SearchForm onSubmit={getPictures} />
+        <Box m={2} />
+        {isFetching ? (
+          <CircularProgress size={70} disableShrink />
+        ) : (
+          <>
+            <PicturesList pictures={pictures} />
+            {nothingFound && (
+              <Alert variant="filled" severity="error">
+                We don't found any picture, please try again with another words
+              </Alert>
+            )}
+            {error && (
+              <Alert variant="filled" severity="error">
+                {error}
+              </Alert>
+            )}
+          </>
+        )}
+      </Grid>
+    </Container>
+  );
+};
 
 export default App;
---
+++ src/pixabayConfig.js
@@ -0,0 +1,9 @@
+const API_URL = "https://pixabay.com/api/";
+const API_KEY = "13417145-d0c367819415b077de5e950e3";
+const PER_PAGE = 21;
+const IMAGE_TYPE = "photo";
+
+export const getUrl = (keyWord) =>
+  `${API_URL}?key=${API_KEY}&q=${encodeURIComponent(
+    keyWord
+  )}&image_type=${IMAGE_TYPE}&per_page=${PER_PAGE}`;
