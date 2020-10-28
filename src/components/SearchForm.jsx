89cc8dc83d0ab382bd8a4c1dc11508325575f682
yurassic <yuriynyay@gmail.com>
2020-10-28 12:51:55 +0200

    Add search form

---
+++ src/components/SearchForm.jsx
@@ -0,0 +1,39 @@
+import { useRef } from "react";
+import PropTypes from "prop-types";
+import TextField from "@material-ui/core/TextField";
+import InputAdornment from "@material-ui/core/InputAdornment";
+import SearchIcon from "@material-ui/icons/Search";
+
+const SearchForm = ({ onSubmit }) => {
+  const ref = useRef(null);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onSubmit(ref?.current?.value);
+  };
+
+  return (
+    <form style={{ width: "100%" }} noValidate onSubmit={handleSubmit}>
+      <TextField
+        id="outlined-basic"
+        label="Type words associated with the pictures you are looking for"
+        fullWidth
+        variant="outlined"
+        inputRef={ref}
+        InputProps={{
+          endAdornment: (
+            <InputAdornment position="end">
+              <SearchIcon />
+            </InputAdornment>
+          ),
+        }}
+      />
+    </form>
+  );
+};
+
+SearchForm.propTypes = {
+  onSubmit: PropTypes.func.isRequired,
+};
+
+export default SearchForm;
